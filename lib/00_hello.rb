0b1472dc302b6a30b990df024f075c97bdad68ae
Youssef <annabiyoussef1986@gmail.com>
2021-01-13 16:30:09 +0100

    exo fini

---
+++ lib/00_hello.rb
@@ -0,0 +1,15 @@
+def ask_first_name
+  puts "quel est ton prénom"
+  first_name = gets.chomp
+  return first_name
+end
+
+def say_hello
+  first_name = ask_first_name
+  puts "bonjour,#{first_name}!" 
+end
+
+say_hello
+
+
+
